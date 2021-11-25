e14c978283b8e80315764ab783b0bc3e2ab350c3
SOp 2019/2020 <sop0304@l040101-ws04.ua.pt>
2021-11-25 22:19:24 +0000

    JF - idk

--- netifstat.sh
+++ netifstat.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+# ------------------------------------ #
+#               FUNCOES                #
+# ------------------------------------ #
+
+# Determina as opções fornecidas
+function get_args() {
+
+    # Por agora, a unica opcao considerada e [T]
+    T=$1
+}
+
+# Recolhe e organiza os dados das interfaces
+function get_data() {
+
+    # Guarda output do comando ifconfig (WIP)
+    data=$(ifconfig | grep ": " | join)
+
+    echo $data 
+}
+
 # Imprime a tabela com os dados
 function print_table() {
-    
-    echo $1
-    # Determina o cabeçalho da tabela
+
+    # Determina o cabecalho da tabela
     if [[ $1 == "0" ]]; then
         echo lol
     else
@@ -14,6 +33,16 @@ function print_table() {
     return 0
 }
 
-print_table $1
+# ------------------------------------ #
+#                 MAIN                 #
+# ------------------------------------ #
+
+get_args $@
+
+get_data
+
+#sleep $T
+
+#get_data
 
 exit 0
