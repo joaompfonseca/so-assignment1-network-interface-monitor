b6082e7d836ee8a229fa0d0aebb71ddaa214ed6c
SOp 2019/2020 <sop0304@l040101-ws04.ua.pt>
2021-12-01 22:24:54 +0000

    JF - finished my part

--- netifstat.sh
+++ netifstat.sh
@@ -4,7 +4,7 @@
 #               FUNÇÕES                #
 # ------------------------------------ #
 
-# Valida, determina e guarda em variáveis as opções e argumentos passados ao script
+# Determina, valida e guarda em variáveis as opções e argumentos passados ao script
 function get_args() {
 
     # Por agora, a única opção considerada é "s"
@@ -15,20 +15,25 @@ function get_args() {
 function get_data() {
 
     # Recolhe o output do comando "ifconfig" num array cujas entradas correspondem a uma linha
-    OLDIFS=$IFS
     IFS=$'\n'
     data=($(ifconfig -a | awk '$1=$1'))
-    IFS=$OLDIFS
+    unset IFS
 
     # Guarda os dados pretendidos em arrays
     if_names=($(printf "%s\n" "${data[@]}" | grep ": " | cut -d: -f1))
     if_TX=($(printf "%s\n" "${data[@]}" | grep "TX.*bytes" | cut -d" " -f5))
     if_RX=($(printf "%s\n" "${data[@]}" | grep "RX.*bytes" | cut -d" " -f5))
 
+    # Não existem interfaces
+    [[ ${#if_names[@]} -eq 0 ]] && throw_error 20
+
     # Validação do filtro das interfaces
     (for int in ${if_names[@]}; do
         [[ $int =~ $if_filter ]] && break
-    done) || throw_error 20 "$if_filter" # Nenhuma interface passou no filtro
+    done) || throw_error 21 "$if_filter" # Nenhuma interface passou no filtro
+
+    # Determina o número máximo de interfaces a apresentar
+    [[ $if_nMax -eq 0 || $if_nMax -gt ${#if_names[@]} ]] && if_nMax=${#if_names[@]}
 
     return 0
 }
@@ -85,42 +90,56 @@ function sort_data() {
 
 # Imprime na consola a tabela formatada com os dados nos arrays
 function print_table() {
-
     # Formato do cabeçalho
     header="%-10s %8s %8s %8s %8s"
     header_loop="%8s %8s"
     # Formato das linhas
-    line="%-10s %8d %8d %8.1f %8.1f"
-    line_loop="%8d %8d"
+    line=$([[ $b -eq 1 ]] && echo "%-10s %8d %8d %8.1f %8.1f" || echo "%-10s %8.1f %8.1f %8.1f %8.1f")
+    line_loop=$([[ $b -eq 1 ]] && echo "%8d %8d" || echo "%8.1f %8.1f")
+
+    # Conversão dos dados de bytes para kilobytes ou megabytes, especificado nos argumentos
+    conv=$(($kb * 1024 + $mb * 1024 * 1024))
+    for ((i = 0; i < ${#if_names[@]}; i++)); do
+        if_TX_conv[i]=$([[ $b -eq 1 ]] && echo ${if_RX[i]} || echo ${if_TX[i]} / $conv | bc -l)
+        if_RX_conv[i]=$([[ $b -eq 1 ]] && echo ${if_RX[i]} || echo ${if_RX[i]} / $conv | bc -l)
+        if_TR_conv[i]=$([[ $b -eq 1 ]] && echo ${if_TR[i]} || echo ${if_TR[i]} / $conv | bc -l)
+        if_RR_conv[i]=$([[ $b -eq 1 ]] && echo ${if_RR[i]} || echo ${if_RR[i]} / $conv | bc -l)
+        if_TXTOT_conv[i]=$([[ $b -eq 1 ]] && echo ${if_TXTOT[i]} || echo ${if_TXTOT[i]} / $conv | bc -l)
+        if_RXTOT_conv[i]=$([[ $b -eq 1 ]] && echo ${if_RXTOT[i]} || echo ${if_RXTOT[i]} / $conv | bc -l)
+    done
 
     # Imprime o cabeçalho
-    printf "$header" "NETIF" "TX" "RX" "TRATE" "RRATE"
-    [[ $loop -eq 1 ]] && printf " $header_loop" "TXTOT" "RXTOT"
+    [[ $cycle -eq 0 ]] && printf "$header" "NETIF" "TX" "RX" "TRATE" "RRATE"
+    [[ $cycle -eq 0 && $loop -eq 1 ]] && printf " $header_loop" "TXTOT" "RXTOT"
     printf "\n"
 
-    # Imprime as linhas
-    for ((i = 0; i < ${#if_names[@]}; i++)); do
-        # A interface não passou pelo filtro
-        ! [[ ${if_names[i]} =~ $if_filter ]] && continue
+    # i - itera pelas interfaces; if_n - número de interfaces impressas
+    for ((i = 0, if_n = 0; i < ${#if_names[@]} && if_n < $if_nMax; i++)); do
+        # Filtragem da interface
+        ! [[ ${if_names[i]} =~ $if_filter ]] && continue || ((if_n++))
 
-        printf "$line" "${if_names[i]}" ${if_TX[i]} ${if_RX[i]} ${if_TR[i]} ${if_RR[i]}
-        [[ $loop -eq 1 ]] && printf " $line_loop" ${if_TXTOT[i]} ${if_RXTOT[i]}
+        # Imprime a linha
+        printf "$line" "${if_names[i]}" ${if_TX_conv[i]} ${if_RX_conv[i]} ${if_TR_conv[i]} ${if_RR_conv[i]}
+        [[ $loop -eq 1 ]] && printf " $line_loop" ${if_TXTOT_conv[i]} ${if_RXTOT_conv[i]}
         printf "\n"
     done
+
+    return 0
 }
 
 # Termina o programa com erro, imprime a causa na consola e retorna o valor de saída correspondente
-# 1 = código do erro
-# 2 = dado que causou o erro
+# $1 - código do erro
+# $2 - dado que causou o erro
 function throw_error() {
 
     case $1 in
     1*)
-        printf "erro de argumentos: "$2""
+        printf "erro de argumentos [$1]: "
         ;;
     2*)
-        printf "erro de execução: $2"
-        [[ $1 -eq 20 ]] && printf ": o filtro fornecido não encontrou nenhuma interface válida"
+        printf "erro de execução [$1]: "
+        [[ $1 -eq 20 ]] && printf "nenhuma interface foi detetada no sistema"
+        [[ $1 -eq 21 ]] && printf "$2: o filtro fornecido não encontrou nenhuma interface válida"
         ;;
     esac
     printf "\n"
@@ -132,9 +151,9 @@ function throw_error() {
 #                 MAIN                 #
 # ------------------------------------ #
 
-time=10
+# Inicialização das variáveis com os valores por defeito
 if_filter=".*"
-if_nMax=0 # Definido dentro da função get_data()
+if_nMax=0 # Depende do número de interfaces, logo é definido na função get_data()
 b=1
 kb=0
 mb=0
@@ -144,19 +163,50 @@ sTR=0
 sRR=0
 sRev=0
 loop=0
+time=0
 
-if_names=("eth0" "wlan" "lo")
-if_TX=(123456 3223 456)
-if_RX=(23456 904 234)
-if_TR=(12345.6 322.3 45.6)
-if_RR=(2345.6 23.4 90.4)
-if_TXTOT=(123456 3223 456)
-if_RXTOT=(23456 904 234)
+if_names=() #("eth0" "wlan" "lo")
+if_TX=() #(123456 3223 456)
+if_RX=() #(23456 904 234)
+if_TR=() #(12345.6 322.3 45.6)
+if_RR=() #(2345.6 23.4 90.4)
+if_TXTOT=() #(123456 3223 456)
+if_RXTOT=() #(23456 904 234)
 
-get_data
+get_args
 
-sort_data
+# Fluxo de execução principal
+cycle=0
+while true; do
+    # 1ª recolha
+    get_data
+    if_TX_prev=(${if_TX[@]})
+    if_RX_prev=(${if_RX[@]})
 
-print_table
+    # Tempo entre recolhas
+    sleep $time
+
+    # 2ª recolha
+    get_data
+    if_TX_curr=(${if_TX[@]})
+    if_RX_curr=(${if_RX[@]})
+
+    # Cálculo dos valores
+    for ((i = 0; i < ${#if_names[@]}; i++)); do
+        if_TX[i]=$((${if_TX_curr[i]} - ${if_TX_prev[i]}))
+        if_RX[i]=$((${if_RX_curr[i]} - ${if_RX_prev[i]}))
+        if_TR[i]=$(echo ${if_TX[i]} / $time | bc -l)
+        if_RR[i]=$(echo ${if_RX[i]} / $time | bc -l)
+        if_TXTOT[i]=$((${if_TXTOT[i]} + ${if_TX[i]}))
+        if_RXTOT[i]=$((${if_RXTOT[i]} + ${if_RX[i]}))
+    done
+
+    sort_data
+
+    print_table
+
+    # Execução em loop
+    [[ $loop -eq 0 ]] && break || ((cycle++))
+done
 
 exit 0
